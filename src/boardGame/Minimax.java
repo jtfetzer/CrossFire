1364b973ff9062589001729373084544235f7bd4
Jonathan T. Fetzer <jonathantfetzer@gmail.com>
2017-04-22 13:41:18 -0700

    Minimax algorithm

--- src/boardGame/Minimax.java
+++ src/boardGame/Minimax.java
@@ -18,10 +18,10 @@ public class Minimax implements Runnable{
 	
 	public static void min(BoardNode node, int depth){
 
-		if(Connect4.SHOW_BOARDS){
+		if(CrossFire.SHOW_BOARDS){
 			System.out.println("-------MIN------- Depth: " + depth + " " + node.lastMove.moveString);
 			node.printBoard();
-			if(!Connect4.SHOW_MOVES_MINIMAX){
+			if(!CrossFire.SHOW_MOVES_MINIMAX){
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -30,21 +30,21 @@ public class Minimax implements Runnable{
 			}
 		}
 		
-		if(node.lastMove.getBestMoveValue() == Connect4.MAX_WINS){
-			node.value = Connect4.MAX_WINS;
+		if(node.lastMove.getBestMoveValue() == CrossFire.MAX_WINS){
+			node.value = CrossFire.MAX_WINS;
 			node.maxWinDepth = depth;
 			return;
 		}
 
-		if(depth == Connect4.MAX_DEPTH || node.getNumEmptySpaces() == 0){
+		if(depth == CrossFire.MAX_DEPTH || node.getNumEmptySpaces() == 0){
 			return;
 		}
 		
-		MoveSet attacks = Connect4.getMostPromisingMoves(node, Connect4.ATTACK_MOVES);
-		MoveSet blocks = Connect4.getMostPromisingMoves(node, Connect4.BLOCK_MOVES);
-		MoveSet bestMoves = blocks.addAll(attacks).reduceMin(Connect4.MAX_WIDTH / 2);
+		MoveSet attacks = CrossFire.getMostPromisingMoves(node, CrossFire.ATTACK_MOVES);
+		MoveSet blocks = CrossFire.getMostPromisingMoves(node, CrossFire.BLOCK_MOVES);
+		MoveSet bestMoves = blocks.addAll(attacks).reduceMin(CrossFire.MAX_WIDTH / 2);
 
-		if(Connect4.SHOW_MOVES_MINIMAX){
+		if(CrossFire.SHOW_MOVES_MINIMAX){
 			bestMoves.print("BestMoves");
 			System.out.println("Press enter to continue:");
 			in.nextLine();
@@ -54,13 +54,13 @@ public class Minimax implements Runnable{
 		
 		for(int j = 0; j < node.size(); j++){ // most promising nodes
 			max(node.getChild(j), depth + 1);
-			if(node.getChild(j).value == Connect4.MAX_WINS){
+			if(node.getChild(j).value == CrossFire.MAX_WINS){
 				if(node.maxWinDepth < node.getChild(j).maxWinDepth){ // Find longest loss path
 					node.maxWinDepth = node.getChild(j).maxWinDepth; 
 				}
-			} else if(node.getChild(j).value == Connect4.MIN_WINS){
+			} else if(node.getChild(j).value == CrossFire.MIN_WINS){
 				node.minWinDepth = node.getChild(j).minWinDepth;
-				node.value = Connect4.MIN_WINS;
+				node.value = CrossFire.MIN_WINS;
 				node.removeAllChildNodesExcept(node.getChild(j));
 				return;
 			} 
@@ -73,10 +73,10 @@ public class Minimax implements Runnable{
 	
 	public static void max(BoardNode node, int depth){
 
-		if(Connect4.SHOW_BOARDS){
+		if(CrossFire.SHOW_BOARDS){
 			System.out.println("-------MAX------- Depth: " + depth + " " + node.lastMove.moveString);
 			node.printBoard();
-			if(!Connect4.SHOW_MOVES_MINIMAX){
+			if(!CrossFire.SHOW_MOVES_MINIMAX){
 				try {
 					Thread.sleep(1000);
 				} catch (InterruptedException e) {
@@ -85,8 +85,8 @@ public class Minimax implements Runnable{
 			}
 		}
 		
-		if(node.lastMove.getBestMoveValue() == Connect4.MIN_WINS){
-			node.value = Connect4.MIN_WINS;
+		if(node.lastMove.getBestMoveValue() == CrossFire.MIN_WINS){
+			node.value = CrossFire.MIN_WINS;
 			node.minWinDepth = depth;
 			return;
 		}
